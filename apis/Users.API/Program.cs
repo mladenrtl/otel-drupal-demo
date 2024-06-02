23328fd6d8f02ad9de0b098f363c8a37d8e12f88
Fausto David Suarez Rosario <fausto.suarez.rosario@rtl.nl>
2024-06-02 18:59:49 +0200

    Add structured logging

--- apis/Users.API/Program.cs
+++ apis/Users.API/Program.cs
@@ -1,3 +1,4 @@
+using OpenTelemetry.Exporter;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
@@ -16,14 +17,13 @@ builder.Services.AddOpenTelemetry()
   .ConfigureResource(configureResource)
   .WithTracing(b =>
   {
-    b.AddAspNetCoreInstrumentation(o => { o.RecordException = true; })
+    b.AddAspNetCoreInstrumentation()
       .AddSource("MongoDB.Driver.Core.Extensions.DiagnosticSources")
       .AddOtlpExporter(o =>
       {
         // For demo purposes, use the simple processor.
         o.ExportProcessorType = ExportProcessorType.Simple;
         o.Endpoint = new Uri(builder.Configuration.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317")!);
-
       });
   });
 
@@ -34,7 +34,19 @@ builder.Logging.AddOpenTelemetry(options =>
   var resourceBuilder = ResourceBuilder.CreateDefault();
   configureResource(resourceBuilder);
   options.SetResourceBuilder(resourceBuilder);
-  options.AddConsoleExporter();
+  options.AddOtlpExporter(otlpOptions =>
+  {
+    // For demo purposes, use the simple processor.
+    otlpOptions.ExportProcessorType = ExportProcessorType.Simple;
+    otlpOptions.Protocol = OtlpExportProtocol.HttpProtobuf;
+    otlpOptions.Endpoint =
+      new Uri(builder.Configuration.GetValue("OtlpLogs:Endpoint", defaultValue: "http://localhost:3100/otlp/v1/logs")!);
+
+    if (builder.Environment.IsDevelopment())
+    {
+      options.AddConsoleExporter();
+    }
+  });
 });
 
 // Add services to the container.
