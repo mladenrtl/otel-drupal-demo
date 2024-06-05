c30c85aad5d2fd62b65737f6860afc92c4c919dd
Tadas Rimkus <dev.tadas.rimkus@gmail.com>
2024-06-05 13:50:18 +0200

    Add frontend Dockerfile

--- frontend/server.js
+++ frontend/server.js
@@ -6,7 +6,7 @@ const sdk = require('./tracing');
 
 const dev = process.env.NODE_ENV !== 'production';
 const hostname = 'localhost';
-const port = 3003;
+const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
