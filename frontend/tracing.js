5960c3095de8dbdb533f67eb3917c17723f58071
Mladen <mladen.stankovic@rtl.nl>
2024-06-10 10:42:24 +0200

    Change name of the project

--- frontend/tracing.js
+++ frontend/tracing.js
@@ -3,19 +3,12 @@
 const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api');
 
 const opentelemetry= require('@opentelemetry/sdk-node');
-const { Resource } = require('@opentelemetry/resources');
-const {
-  SEMRESATTRS_SERVICE_NAME,
-} = require('@opentelemetry/semantic-conventions');
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
 
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 // enable all auto-instrumentations from the meta package
 const sdk = new opentelemetry.NodeSDK({
-  resource: new Resource({
-    [SEMRESATTRS_SERVICE_NAME]: `ddev-rtl-web-frontend`,
-  }),
   instrumentations: [getNodeAutoInstrumentations(
     {
       "@opentelemetry/instrumentation-fs": {
